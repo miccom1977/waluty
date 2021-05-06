d8e309b5c8926788554fa95d9111bcf1f66c11ff
Misiek M <marcakster@gmail.com>
2021-05-07 01:35:09 +0200

    add login / logout function

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('index');
+        }
+        
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
